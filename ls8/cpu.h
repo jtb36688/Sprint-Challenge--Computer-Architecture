2bd0ee88ef7a0f39732ecb85a6bd20e9557d9b08
Jacob Bryan <jtb36688@gmail.com>
2019-06-28 11:24:16 -0400

    Added the binary definitions for CMP JMP JEQ JNE instructions

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -33,6 +33,9 @@ enum alu_op {
 #define CALL 0b01010000
 #define RET  0b00010001
 #define JMP  0b01010100
+#define CMP  0b10100111
+#define JEQ  0b01010101
+#define JNE  0b01010110
 
 
 // TODO: more instructions here. These can be used in cpu_run().
